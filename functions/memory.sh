30875ec6b6273a1083520689322ca636fdd8e4bd
Vincent S. Cojot <vincent@cojot.name>
2016-04-17 09:07:15 -0400

    Fix formatting..

--- functions/memory.sh
+++ functions/memory.sh
@@ -19,16 +19,16 @@
 get_available_memory() {
 	local total_memory
 	case $(uname) in
-	Linux | CYGWIN*)
-	total_memory=$(LANG=C free | grep Mem | awk '{print $2}')
-;;
-Darwin)
-total_memory=$(sysctl -n hw.memsize)
-total_memory=$(( ${total_memory} / 1024 ))
-;;
-*)
-total_memory="-1"
-;;
-esac
-echo ${total_memory}
+		Linux | CYGWIN*)
+			total_memory=$(LANG=C free | grep Mem | awk '{print $2}')
+			;;
+		Darwin)
+			total_memory=$(sysctl -n hw.memsize)
+			total_memory=$(( ${total_memory} / 1024 ))
+			;;
+		*)
+			total_memory="-1"
+			;;
+	esac
+	echo ${total_memory}
 }
