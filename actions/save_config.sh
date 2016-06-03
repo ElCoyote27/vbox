00e4644698348e936caef19447f71afcef1e00a6
Vincent S. Cojot <vcojot@redhat.com>
2016-06-03 13:13:03 -0400

    update

--- actions/save_config.sh
+++ actions/save_config.sh
@@ -11,8 +11,8 @@ MYCONF=".config"
 
 /bin/rm -f ${MYCONF}
 for value in \
-	vm_nic_type vm_master_cpu_cores \
-	vm_master_memory_mb vm_master_disk_mb \
+	vm_boot_nic_type vm_default_nic_type \
+	vm_master_cpu_cores vm_master_memory_mb vm_master_disk_mb \
 	cluster_size vm_slave_cpu_default vm_slave_memory_default \
 	vm_slave_first_disk_mb
 do
