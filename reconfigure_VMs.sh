d3270dfdd1f0d3981914e652471a67e42813ed52
Vincent S. Cojot <vincent@cojot.name>
2017-03-22 10:18:28 -0400

    Update

--- reconfigure_VMs.sh
+++ reconfigure_VMs.sh
@@ -7,12 +7,15 @@ source ./functions/memory.sh
 
 total_memory=$(get_available_memory)
 
+#vboxmanage setextradata osp-baremetal-1 "VBoxInternal/Devices/ahci/0/LUN0/Config/IgnoreFlush" 0
+
 vbox_vm_flags=""
 vbox_vm_flags="${vbox_vm_flags} --nestedpaging on"
 vbox_vm_flags="${vbox_vm_flags} --vtxvpid on"
 vbox_vm_flags="${vbox_vm_flags} --vtxux on"
 vbox_vm_flags="${vbox_vm_flags} --largepages on"
 vbox_vm_flags="${vbox_vm_flags} --chipset piix3"
+#vbox_vm_flags="${vbox_vm_flags} --chipset ich9"
 vbox_vm_flags="${vbox_vm_flags} --largepages on"
 vbox_vm_flags="${vbox_vm_flags} --pae off"
 vbox_vm_flags="${vbox_vm_flags} --apic on"
@@ -44,31 +47,34 @@ for i in {1..16}
 do
 	name="osp-baremetal-${i}"
 	echo "(II) Reconfiguring VM ${name}..."
-	VBoxManage modifyvm ${name} ${vbox_vm_flags}
+	vboxmanage modifyvm ${name} ${vbox_vm_flags}
 done
 
 for i in 1
 do
 	name="osp-instack"
 	echo "(II) Reconfiguring VM ${name}..."
-	VBoxManage modifyvm ${name} ${vbox_vm_flags}
+	vboxmanage modifyvm ${name} ${vbox_vm_flags}
 done
 
 # RAM/cpus might be different for controllers...
-echo "(II) Reconfiguring Controllers : --memory ${ctrl_mem} --cpus ${ctrl_cpus}, disk0 --resize ${vm_slave_first_disk_mb}..."
+echo "(II) Reconfiguring Controllers : --memory ${ctrl_mem} --cpus ${ctrl_cpus}, ..."
 vboxmanage modifyvm osp-instack --memory ${ctrl_mem} --cpus ${ctrl_cpus} 
 for i in $(seq 1 3); do
 	vboxmanage modifyvm osp-baremetal-${i} --memory ${ctrl_mem} --cpus ${ctrl_cpus}
-	disk_path=$(vboxmanage showvminfo osp-baremetal-${i}|grep 'SATA (0, 0)'|awk '{ print $4}')
-	vboxmanage modifyhd ${disk_path} --resize ${vm_slave_first_disk_mb}
-	vboxmanage storagectl osp-baremetal-${i} --name SATA --hostiocache on
 done
 
-echo "(II) Reconfiguring Other Nodes : --memory ${cmpt_mem} --cpus ${cmpt_cpus}, disk0 --resize ${vm_slave_first_disk_mb}..."
+echo "(II) Reconfiguring Other Nodes : --memory ${cmpt_mem} --cpus ${cmpt_cpus}, ..."
 # Computes/ceph/etc...
 for i in $(seq 4 16); do
 	vboxmanage modifyvm osp-baremetal-${i} --memory ${cmpt_mem} --cpus ${cmpt_cpus}
+done
+
+for i in $(seq 1 16); do
+	echo "(II) Reconfiguring node osp-baremetal-${i}.. : --hostiocache on, IgnoreFlush=0, DmiExposeMemoryTable=1, disk0 --resize ${vm_slave_first_disk_mb}, ... "
 	disk_path=$(vboxmanage showvminfo osp-baremetal-${i}|grep 'SATA (0, 0)'|awk '{ print $4}')
 	vboxmanage modifyhd ${disk_path} --resize ${vm_slave_first_disk_mb}
 	vboxmanage storagectl osp-baremetal-${i} --name SATA --hostiocache on
+	vboxmanage setextradata osp-baremetal-${i} "VBoxInternal/Devices/ahci/0/LUN0/Config/IgnoreFlush" 0
+	vboxmanage setextradata osp-baremetal-${i} "VBoxInternal/Devices/pcbios/0/Config/DmiExposeMemoryTable" "1"
 done
